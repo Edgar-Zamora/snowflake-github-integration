0a209dd57a5eba073e3bfeef1060aebddca1c5ec
EZ1501540 <edgarzamora2012@hotmail.com>
2026-08-12 21:07:54 +0000

    making changes from snowflake itself

--- hello_world.sql
+++ hello_world.sql
@@ -1,3 +1,4 @@
 SELECT 
     'Hello World' AS hello_world,
-    'Lets add another column' AS new_column;
+    'Lets add another column' AS new_column,
+    'Yankees are the best' AS another_new_column;
